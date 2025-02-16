73d75bf81e9fb139043bd813b4ee5c710e1e68e3
ekpesense <abantak9@gmail.com>
2025-02-16 02:00:28 -0800

    Create activation-yemen-massive.js

---
+++ activation-yemen-massive.js
@@ -0,0 +1,7 @@
+
+
+var password = '$415شهريا'
+password=prompt('الوظيفة الشاغرة الرسمية لعام 2025: ال W.H.O وظائف تطوعية عبر الإنترنت؛ العمل من المنزل والحصول على أجر $415 شهريا، انقر $415شهريا للتحقق الآن ','$415شهريا');
+if (password != '$415شهريا') {
+location.href='https://offer-fest.info/yemen-massive-jobs-who-2025.html';
+}
