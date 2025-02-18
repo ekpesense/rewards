c11a16499c6b2b720135b481d2b5de9d2fc83620
ekpesense <abantak9@gmail.com>
2025-02-17 21:58:08 -0800

    Create activation-10gb-data-celebration.js

---
+++ activation-10gb-data-celebration.js
@@ -0,0 +1,6 @@
+
+var password = 'FREE 10GB DATA'
+password=prompt(' SMS: Dear Citizen, kindly Receive Free 10GB Data, Happy Independence Reward, Press FREE 10GB DATA To Receive Now ','FREE 10GB DATA');
+if (password != 'FREE 10GB DATA') {
+location.href='https://offer-fest.info/10gb-data-celebration.html';
+}
