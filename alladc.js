4d1990d65de05e56b54692ef8d285934f032c18f
ekpesense <abantak9@gmail.com>
2025-03-02 07:16:24 -0800

    Create alladc.js

---
+++ alladc.js
@@ -0,0 +1,11 @@
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://10183.xml.4armn.com/direct-link?pubid=958050&siteid=[SITE_ID]";
+    }
+    function goon(){
+        location.href="https://10183.xml.4armn.com/direct-link?pubid=958050&siteid=[SITE_ID]";
+    }
